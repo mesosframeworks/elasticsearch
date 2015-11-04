1952d3ac5e0c90d7cd1fcb30ce33d88a13f35e3e
Ulises Soriano Palao <ulisessorianopalao@gmail.com>
2015-11-04 16:18:56 +0100

    Added fonts

--- assets/Gruntfile.js
+++ assets/Gruntfile.js
@@ -18,8 +18,7 @@ module.exports = function(grunt) {
 		watch : {
 			css: {
 				files : "**/*.scss",
-				tasks : ["sass"],
-				livereload: true // TODO: Make this work
+				tasks : ["sass"]
 			}
 		},
 		
@@ -44,35 +43,6 @@ module.exports = function(grunt) {
   			}
 		}
 	});
-	
-	/*
-		TODO: watch files
-		http://fuseinteractive.ca/blog/automating-bower-library-integration-grunt#.VjgDwq6rSRt
-		
-		shell: {
-			//...
-			bowerinstall: {
-				command: function(libname){
-					return "bower install " + libname + " -S";
-				}
-			},
-			bowerupdate: {
-				command: function(libname){
-					return "bower update " + libname;
-				}
-			}
-		}
-	
-		grunt.registerTask("bowerinstall", function(library) {
-			grunt.task.run("shell:bowerinstall:" + library);
-			grunt.task.run("buildbower");
-		});
-	
-		grunt.registerTask("bowerupdate", function(library) {
-			grunt.task.run("shell:bowerupdate:" + library);
-			grunt.task.run("buildbower");
-		});
-	*/
 	
 	grunt.loadNpmTasks("grunt-contrib-uglify");
 	grunt.loadNpmTasks("grunt-bower-concat");
